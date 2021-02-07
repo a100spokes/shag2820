bda876f041ee7d7b875461897156a7d8245166b8
a100spokes <72028978+a100spokes@users.noreply.github.com>
2021-02-07 20:49:12 +0200

    Delete src directory

--- src/app.js
+++
@@ -1,9 +0,0 @@
-import "@static/scss/app.scss";
-import React from "react";
-import {render} from "react-dom";
-import Core from "@comp/Core";
-
-
-render(<Core />,document.getElementById("app"),()=>{
-    console.log("render")
-})
