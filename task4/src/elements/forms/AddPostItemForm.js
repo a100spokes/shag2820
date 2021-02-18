c95dc93e0de5269322c24569e430e11792d109b5
vato.trane <vato.trane@gmail.com>
2021-02-18 13:03:22 +0300

    test1

---
+++ task4/src/elements/forms/AddPostItemForm.js
@@ -0,0 +1,14 @@
+import React, { useState } from 'react';
+
+const AddPostItemForm = (props) => {
+
+
+    return (
+        <form>
+           <input type={"text"} />
+            <button>add</button>
+        </form>
+    );
+}
+
+export default AddPostItemForm;
