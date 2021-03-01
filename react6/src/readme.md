da67a6b0f2697c65d8e5e479d723ce847936db36
a100spokes <72028978+a100spokes@users.noreply.github.com>
2021-03-01 16:52:04 +0300

    Delete react6 directory

--- react6/src/readme.md
+++
@@ -1,3 +0,0 @@
-https://habr.com/ru/post/498860/
-
-https://rajdee.gitbooks.io/redux-in-russian/content/docs/basics/UsageWithReact.html
--- react6/src/redux/reduces/index.js
+++
@@ -1,8 +0,0 @@
-import {combineReducers} from "redux";
-import Loader from "./loader";
-import Todos from "./todos";
-
-export default  combineReducers({
-    Loader,
-    Todos
-})
