dbba1bfc0df1d21276a40efc739120a2a3370e42
a100spokes <72028978+a100spokes@users.noreply.github.com>
2021-03-11 19:45:44 +0200

    Delete task 3 directory

--- task 3/src/components/loader/Loader.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import "./Loader.style.scss"
-
-export default function Loader() {
-
-    return(
-        <div class="lds-facebook"><div></div><div></div><div></div></div>
-    )
-}
