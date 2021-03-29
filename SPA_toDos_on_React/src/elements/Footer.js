2ffb1156422ae3d6fdf0d31da1dc0da7dc3186d9
vato.trane <vato.trane@gmail.com>
2021-03-29 15:28:55 +0300

    Features/add timer

--- SPA_toDos_on_React/src/elements/Footer.js
+++ SPA_toDos_on_React/src/elements/Footer.js
@@ -1,26 +1,18 @@
 import React from "react";
-import Logo from "@comp/logo/Logo";
+import Timer from "@comp/timer/Timer";
 import "./style.scss";
 
 export default function Footer() {
-    const title = "hello";
-    const titleNew = title.split("").join(" ");
-
-
+    
     return(
         
         <footer>
             <div class="container">
             <div class="glitch" data-text="Made on React">Made on React</div>
-  <div class="glow">Made on React</div> 
-  </div>  
+              <div class="glow">Made on React</div> 
+            </div>  
+                <Timer/>  
         </footer>      
-    )
-    /* return(
-        <footer>
-            <Logo bgColor={"green"} text={"logo footer"} />
-            <div>{titleNew}</div>
-            <div>{Date.now() + title}</div>
-        </footer>
-    ) */
+    )   
 }
+
