2aef24496762cf34fa0759e7f7a12048b944f526
vato.trane <vato.trane@gmail.com>
2021-03-29 13:27:02 +0300

    Features/add daytime on home

--- SPA_toDos_on_React/src/pages/home/home.js
+++ SPA_toDos_on_React/src/pages/home/home.js
@@ -3,6 +3,28 @@ import React, {Component, Fragment} from "react";
 import {Link} from "react-router-dom";
 import {Jumbotron} from 'reactstrap';
 
+let timeNow ='';
+getDateTime();
+function getDateTime() {
+
+    let date = new Date();
+    let hour = date.getHours();
+    
+    if (hour<=6){
+        timeNow = "night";
+    }
+    else if (hour>6 && hour<=12){
+        timeNow = "morning";
+    }
+    else if (hour>12 && hour<=18){
+        timeNow = "day";
+    }
+    else{
+        timeNow ="evening";
+    }
+    return timeNow;    
+}
+
 export default class Home extends Component{
     constructor(props) {
         super(props);
@@ -13,7 +35,8 @@ export default class Home extends Component{
         return(
             <Fragment>
                  <Jumbotron className="jumBody">
-                    <h1 className="display-3">Hello, world!</h1>
+                    {/* <h1 className="display-3">Hello, world!</h1> */}
+                    <h1 className="display-3">Good {timeNow}, world!</h1>
                     <p className="lead">This is my firs SPA, made with REACT =)</p>
                     <hr className="my-2" />
                     <p>It uses   <a target="blank" href="https://jsonplaceholder.typicode.com/"> <font color="#4c1d95"><b>json  placeholder</b></font></a> api to form  <span><Link class="hover-shadow hover-color" to={"/posts"}><span>p</span><span>o</span><span>s</span><span>t</span><span>s</span></Link >
@@ -24,5 +47,7 @@ export default class Home extends Component{
         )
     }
 }
+
+
 
 
