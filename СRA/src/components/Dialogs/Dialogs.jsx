1ceb55970e28aedd0da4d7bdabc5c0744d7165f3
a100spokes <vato.trane@gmail.com>
2021-04-15 22:52:30 +0200

    add dialogs routs

--- СRA/src/components/Dialogs/Dialogs.jsx
+++ СRA/src/components/Dialogs/Dialogs.jsx
@@ -1,16 +1,31 @@
 import React from "react";
 import classes from "./Dialogs.module.css";
+import { NavLink } from "react-router-dom";
 
 const Dialogs = (props) => {
   return (
     <div className={classes.dialogs} >
       <div className={classes.dialogsItem}>
-        <div className={classes.dialog + ' ' + classes.active}>Dmitriy</div>
-        <div className={classes.dialog}>Ivan</div>
-        <div className={classes.dialog}>Jill</div>
-        <div className={classes.dialog}>Rick</div>
-        <div className={classes.dialog}>Oksana</div>
+        <div className={classes.dialog + ' ' + classes.active}>
+          <NavLink to="/dialogs/1">Dmitriy</NavLink>
+          </div>
+        <div className={classes.dialog}>
+        <NavLink to="/dialogs/2">Ivan</NavLink>
+        </div>
+        <div className={classes.dialog}>
+        <NavLink to="/dialogs/3">Jill</NavLink>
+        </div>
+        <div className={classes.dialog}>
+        <NavLink to="/dialogs/4">Rick</NavLink>
+          </div>
+        <div className={classes.dialog}>
+        <NavLink to="/dialogs/5">Oksana</NavLink>
+          </div>
+        <div className={classes.dialog}>
+        <NavLink to="/dialogs/6">Dan</NavLink>
+          </div>
       </div>
+
     <div className={classes.messages}>
       <div className={classes.message}>Hi</div>
       <div className={classes.message}>How are You</div>
