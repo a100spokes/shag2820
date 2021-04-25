4d6481d7d34a37164e1563f1ea578faab43b3e01
a100spokes <vato.trane@gmail.com>
2021-04-25 23:04:37 +0200

    add dialogs mapping

--- СRA/src/components/Profile/MyPosts/MyPosts.jsx
+++ СRA/src/components/Profile/MyPosts/MyPosts.jsx
@@ -3,6 +3,14 @@ import classes from "./MyPosts.module.css";
 import Post from "./Post/Post";
 
 const MyPosts = () => {
+
+  let posts = [
+    { id: 1, message: "Hi, how are you?", likesCount: "12" },
+    { id: 2, message: "It`s my first post", likesCount: 10 },
+  ];
+
+  let postsElements = posts.map(post =><Post message={post.message} count={post.likesCount} />)
+
   return (
     <div className={classes.postBlock}>
       <h3>My post</h3>
@@ -15,8 +23,7 @@ const MyPosts = () => {
         </div>
       </div>
       <div className={classes.posts}>
-        <Post message="Hi, how are you?" count="13" />
-        <Post message="It`s my first post" count="3" />
+        {postsElements}
       </div>
     </div>
   );
