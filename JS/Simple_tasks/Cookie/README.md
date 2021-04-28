cae404be7fd8326a1bd77c3ccc75d8740710e70e
vato.trane <vato.trane@gmail.com>
2021-04-28 10:28:25 +0300

    cookie readme

---
+++ JS/Simple_tasks/Cookie/README.md
@@ -0,0 +1,11 @@
+# Simple auth form made with using cookies. 
+
+ - the form is using cookies to remember user login and visits count
+ - using RegExp  
+
+# Quick Start
+
+```
+open index.html with Live Server
+
+```
--- JS/Simple_tasks/Cookie/js/script.js
+++ JS/Simple_tasks/Cookie/js/script.js
@@ -1,7 +1,9 @@
 "use strict";
 window.onload = function () {
-  let re = [["login", /^[a-zA-z0-9._-]{5,15}$/, "Please enter 5-15 symbols"]];
 
+  let registration = [
+    ["login", /^[a-zA-z0-9._-]{5,15}$/, "Please enter 5-15 symbols"]
+  ];
   let form = document.querySelector(`form[name="auth"]`);
 
   function setCookie1(visits) {
@@ -23,28 +25,23 @@ window.onload = function () {
       ";";
   }
 
+
   if ("" == document.cookie) {
     // Инициализация cookie.
     if (checkCookie("name")) {
       setCookie1(1);
-      /* let div1 = ce("div", `визитов: 0`, "vis");
-            document.body.append(div1); */
-      //  document.write("<H3>Поздравляю Вас с первым посещением моего сайта.</H3>");
     }
   } else {
     var cookies = parseCookie();
     // Вывод приветствия, числа посещений и увеличение числа посещений на 1.
     isNaN(cookies.visits) ? (cookies.visits = 1) : null;
-    let div1 = ce("div", "visits: " + cookies.visits++, "vis1");
+    let div1 = CreateElement("div", "visits: " + cookies.visits++, "vis1");
     document.body.prepend(div1);
-    //    document.write("<H4>Мы снова рады видеть Вас на моем сайте! Число лично ваших посещений - " +
-    //       cookies.visits++ + " !</H4>");
     // Вывод даты последнего посещения.
     if (cookies.LastVisit != undefined) {
-      let div2 = ce("div", "last visit: " + cookies.LastVisit, "vis");
+      let div2 = CreateElement("div", "last visit: " + cookies.LastVisit, "vis");
       document.body.prepend(div2);
     }
-    //    document.write("<H4>Последний раз Вы были у меня на сайте: " + cookies.LastVisit + ".</H4>");
     // Обновление cookie.
     setCookie1(isNaN(cookies.visits) ? 1 : cookies.visits);
   }
@@ -53,41 +50,34 @@ window.onload = function () {
     form.remove();
     let timePass = new Date().getHours();
     let hello;
-    // console.log(timePass);
-    timePass >= 0 && timePass <= 6 ? (hello = "Доброй ночи,") : null;
-    timePass > 6 && timePass <= 12 ? (hello = "Доброго утра,") : null;
-    timePass > 12 && timePass <= 18 ? (hello = "Добрый день,") : null;
-    timePass > 18 && timePass <= 23 ? (hello = "Добрый вечер,") : null;
+
+    timePass >= 0 && timePass <= 6 ? (hello = "Goodnight,") : null;
+    timePass > 6 && timePass <= 12 ? (hello = "Good morning,") : null;
+    timePass > 12 && timePass <= 18 ? (hello = "Good day,") : null;
+    timePass > 18 && timePass <= 23 ? (hello = "Good evening,") : null;
     document.body.style.backgroundColor = "#00ffcf3d";
-    let div = ce(
+    let div = CreateElement(
       "div",
-      `${hello} ${getCookie(/* "name" ,*/ 2)} с города ${getCookie(6)}!`,
+      `${hello} ${getCookie(2)} from ${getCookie(6)}!`,
       "hello"
     );
-    // let div1 = ce("div", `визитов: ${getCookie(10)}`, "vis");
-    // let div2 = ce("div", `Последнее посещение:${count2}`, "vis");
-    let exit = ce("button", `Выход`, null, "click", exitFn);
+
+    let exit = CreateElement("button", `exit`, null, "click", exitFn);
     document.body.append(div);
     document.body.append(exit);
-    // document.body.append(div1);
-    // document.body.append(div2);
-    // alert(1)
   }
-  //let form = document.forms.auth;
 
   form.addEventListener("submit", (e) => {
     e.preventDefault();
     let data = [];
     let flag = false;
     let city = form.querySelector(`[name="city"] > option:checked`).value;
-    let countDat = 0;
     let login = form.querySelector(`[name="login"]`);
     data.push(validate(login) ? removeError(login) : setError(login));
     data.push(city);
-    // data.push(countDat);
-    let tech = form.getElementsByClassName(`check`);
-    for (let index = 0; index < tech.length; index++) {
-      if (tech[index].checked) {
+    let remmemberCheck = form.getElementsByClassName(`check`);
+    for (let index = 0; index < remmemberCheck.length; index++) {
+      if (remmemberCheck[index].checked) {
         flag = true;
       }
     }
@@ -95,19 +85,18 @@ window.onload = function () {
     if (!globalError() && flag) {
       setCookie("name", data[0], 60);
       setCookie("city", data[1], 60);
-      // setCookie("count1", data[2], 60);
+
       location.reload(true);
       console.log(data);
     } else if (!globalError()) {
-      let qustion = confirm(`Запомнить данные?`);
+      let qustion = confirm(`Do you want to remember your data?`);
       if (qustion == true) {
         setCookie("name", data[0], 60);
         setCookie("city", data[1], 60);
-        // setCookie("count1", data[2], 60);
         location.reload(true);
         console.log(data);
       } else {
-        alert("Страница перезагружена, данные не сохранены.");
+        alert("Page reloaded, all data are cleared.");
         location.reload(true);
       }
     }
@@ -134,14 +123,12 @@ window.onload = function () {
   function setCookie(name, val, time) {
     document.cookie = `${name}=${val};max-age=${time * 60}`;
     var expireDate = new Date();
-    var today = new Date();
+
     expireDate.setDate(365 + expireDate.getDate());
     expireDate.setDate(365 + expireDate.getDate());
   }
 
-  function getCookie(/* name, */ n) {
-    //update !!!!!
-    // return document.cookie.split("=")[n];
+  function getCookie(n) {
     return document.cookie.split(/(=|;)/)[n];
   }
 
@@ -154,8 +141,8 @@ window.onload = function () {
     location.reload(true);
   }
   ///////////////////////////////////////////////////////////////////////////////
-  ///////////////////////////////////////////////////////////////////////////////
-  function ce(name = "div", text, className, event, fn) {
+
+  function CreateElement(name = "div", text, className, event, fn) {
     let elem = document.createElement(name);
     className != undefined ? (elem.className = className) : null;
     text != undefined ? (elem.innerHTML = text) : null;
@@ -165,7 +152,7 @@ window.onload = function () {
 
   function validate(node) {
     let valid = false;
-    re.forEach((item, i) => {
+    registration.forEach((item, i) => {
       if (item[0] == node.name && item[1].test(node.value)) {
         valid = true;
         return true;
@@ -176,7 +163,7 @@ window.onload = function () {
 
   function getErrorMessage(node) {
     let message = null;
-    re.forEach((item, i) => {
+    registration.forEach((item, i) => {
       if (item[0] == node.name) {
         message = item[2];
         return true;
@@ -192,7 +179,7 @@ window.onload = function () {
   function setError(node) {
     if (node.nextElementSibling == null) {
       node.classList.add("error");
-      node.parentElement.append(ce("span", getErrorMessage(node)));
+      node.parentElement.append(CreateElement("span", getErrorMessage(node)));
     }
   }
 
@@ -204,5 +191,5 @@ window.onload = function () {
     return node.value;
   }
 
-  
+
 };
