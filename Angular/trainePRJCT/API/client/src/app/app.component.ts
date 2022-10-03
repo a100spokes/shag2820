aba0113de7db163647e0e2d164b38dae0f3f5305
a100spokes <vato.trane@gmail.com>
2022-10-03 22:59:40 +0300

    End of section 5

--- Angular/trainePRJCT/API/client/src/app/app.component.ts
+++ Angular/trainePRJCT/API/client/src/app/app.component.ts
@@ -1,5 +1,7 @@
+import { AccountService } from './_services/account.service';
 import { Component, OnInit } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { User } from './_models/user';
 
 @Component({
   selector: 'app-root',
@@ -10,11 +12,15 @@ export class AppComponent implements OnInit {
   title = 'The Datting app';
   users: any;
 
-  constructor (public http: HttpClient) {}
-  ngOnInit() {
-   this.getUsers();
+  constructor ( private accountService: AccountService) {}
+  ngOnInit() { 
+   this.setCurrentUser();
   }
 
+  setCurrentUser () {
+    const user: User = JSON.parse(localStorage.getItem('user'));
+    this.accountService.setCurrentUser(user);
+  }
   /* getUsers() {
     this.http.get('https://localhost:5001/api/users').subscribe(response =>{
       this.users = response;
@@ -23,10 +29,5 @@ export class AppComponent implements OnInit {
    })
   } */
 
-  getUsers() {
-    this.http.get('https://localhost:5001/api/users').subscribe({
-      next: response => this.users = response,
-      error: error => console.log(error)
-    })
-  }
+   
 }
